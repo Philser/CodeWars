25500e392b983c714a07c448f6292f058dab0c9f
Philser <pk30.05@hotmail.de>
2019-10-12 15:11:56 +0200

    * Command language now features numbers to define number of command executions * Fixed bugs * Refactored

--- Roboscript2/src/App.kt
+++ Roboscript2/src/App.kt
@@ -38,42 +38,43 @@ class App {
         var currY = 0
         var startX = currX // The 0 x point in the final grid
         var startY = currY // The 0 y point in the final grid
-        var xDimension = 1
-        var yDimension = 1
+        var xDimensionPos = 1
+        var yDimensionPos = 1
+        var xDimensionNeg = 0
+        var yDimensionNeg = 0
         var directionX = 1 // Facing right
         var directionY = 0 // Not facing up/down
 
-        for (token in code) {
-            if (token.equals('F')) {
+        val regex = Regex("([LFR])([0-9]*)")
+        val matches = regex.findAll(code)
+
+        var token = ""
+        var repeat = 0
+        for (match in matches) {
+            token = match.groupValues[1] // Token
+            repeat = if(match.groupValues[2] != "") match.groupValues[2].toInt() else 1
+            if (token == "F") {
                 // Move
-                currX += directionX
-                currY += directionY
-                if (currX < 0 && directionX < 0) startX += 1
-                if (currY < 0 && directionY < 0) startY += 1
-                if (abs(currX) > xDimension) xDimension = abs(currX)
-                if (abs(currY) > yDimension) yDimension = abs(currY)
-            } else if (token.equals('L')) {
-                // Turn left
-                if(directionY == 0) { // Turn facing up- or downwards
-                    directionY = directionX * -1 // Will be the inverse of the curr X
-                    directionX = 0
-                } else { // Turn facing left or right
-                    directionX = directionY
-                    directionY = 0
+                for (i in 0 until repeat) {
+                    currX += directionX
+                    currY += directionY
+                    if (currX < 0 && directionX < 0) startX += 1
+                    if (currY < 0 && directionY < 0) startY += 1
+                    if (currX > xDimensionPos) xDimensionPos = currX // Track the furthest we have moved in positive X direction
+                    if (currX < xDimensionNeg) xDimensionNeg = currX // Track the furthest we have moved in negative X direction
+                    if (currY > yDimensionPos) yDimensionPos = currY
+                    if (currY < yDimensionNeg) yDimensionNeg = currY
                 }
-            } else if (token.equals('R')) {
-                // Turn right
-                if (directionY == 0) { // Turn facing up- or downwards
-                    directionX = directionY
-                    directionY = 0
-                } else { // Turn facing left or right
-                    directionX = directionY * -1 // Will be the inverse of the curr X
-                    directionY = 0
+            } else if (token == "L" || token == "R") {
+                for (i in 0 until repeat) {
+                    val dirPair = getNewDirection(directionX, directionY, token)
+                    directionX = dirPair.first
+                    directionY = dirPair.second
                 }
             }
         }
 
-        return arrayOf(xDimension + 1, yDimension + 1, startX, startY)
+        return arrayOf(abs(xDimensionNeg) + xDimensionPos + 1, abs(yDimensionNeg) + yDimensionPos + 1, startX, startY)
     }
 
     private fun moveRobotOnGrid(code:String, grid: Array<Array<String>>, startX: Int, startY: Int) {
@@ -81,35 +82,65 @@ class App {
         var currY = startY
         var directionX = 1 // Facing right
         var directionY = 0 // Not facing up/down
-        for (token in code) {
-            if (token.equals('F')) {
+
+        val regex = Regex("([LFR])([0-9]*)")
+        val matches = regex.findAll(code)
+
+        var token = ""
+        var repeat = 0
+        for (match in matches) {
+            token = match.groupValues[1] // Token
+            repeat = if(match.groupValues[2] != "") match.groupValues[2].toInt() else 1
+            if (token == "F") {
                 // Move
-                currX += directionX
-                currY += directionY
-                grid[currY][currX] = "*"
-            } else if (token.equals('L')) {
-                // Turn left
-                if(directionY == 0) { // Turn facing up- or downwards
-                    directionY = directionX * -1 // Will be the inverse of the curr X
-                    directionX = 0
-                } else { // Turn facing left or right
-                    directionX = directionY
-                    directionY = 0
+                for (i in 0 until repeat) {
+                    // Move
+                    currX += directionX
+                    currY += directionY
+                    grid[currY][currX] = "*"
                 }
-            } else if (token.equals('R')) {
-                // Turn right
-                if (directionY == 0) { // Turn facing up- or downwards
-                    directionX = directionY
-                    directionY = 0
-                } else { // Turn facing left or right
-                    directionX = directionY * -1 // Will be the inverse of the curr X
-                    directionY = 0
+            } else if (token == "L" || token == "R") {
+                for (i in 0 until repeat) {
+                    grid[currY][currX] = "*"
+                    val dirPair = getNewDirection(directionX, directionY, token)
+                    directionX = dirPair.first
+                    directionY = dirPair.second
                 }
             }
         }
     }
 
+//    private fun navigateOnGrid(code: String, grid: Array<Array<String>>?): Array<Int> {
+//
+//    }
+
+    private fun getNewDirection(currXOrientation: Int, currYOrientation: Int, turn: String): Pair<Int, Int> {
+        var directionX = currXOrientation
+        var directionY = currYOrientation
+        if (turn.equals("L")) {
+            // Turn left
+            if(directionY == 0) { // Turn facing up- or downwards
+                directionY = directionX * -1 // Will be the inverse of the curr X
+                directionX = 0
+            } else { // Turn facing left or right
+                directionX = directionY
+                directionY = 0
+            }
+        } else if (turn.equals("R")) {
+            // Turn right
+            if (directionY == 0) { // Turn facing up- or downwards
+                directionY = directionX
+                directionX = 0
+            } else { // Turn facing left or right
+                directionX = directionY * -1 // Will be the inverse of the curr X
+                directionY = 0
+            }
+        }
+
+        return Pair(directionX, directionY)
+    }
+
     fun main() {
-       System.out.print(execute("FFFFFLFFFFFLFFFFFLFFFFFL"))
+       System.out.print(execute("LF5R1F3R1F3R1F7"))
     }
 }
